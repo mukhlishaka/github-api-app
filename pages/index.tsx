6f3baf9a65476a51ced2bbde13d054d596441c85
mukhlishaka <mukhlishaka@gmail.com>
2022-12-29 09:48:30 +0700

    add feature loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
+import Head from "next/head";
+import { useState } from "react";
 import Button from "../components/Button";
 import Form from "../components/Form";
 import { Header } from "../components/Header";
 import UserList from "../components/UserList";
 
 export default function Home() {
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const onSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true)
+  }
   return (
     <div className="space-y-5">
+      <Head>
+        <title>Search Users App</title>
+      </Head>
       <Header />
-      <form className="flex place-self-center space-x-3">
+      <form className="flex place-self-center space-x-3" onSubmit={onSearchSubmit}>
         <Form />
-        <Button type="submit" isLoading={false} />
+        <Button type="submit" isLoading={isLoading} />
       </form>
       <UserList />
     </div>
